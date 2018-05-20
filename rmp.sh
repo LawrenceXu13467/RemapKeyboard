db722cb754ea9b55cdb8d2c0d730f0b33166ba5e
Lawrence Xu <x15026471536@icloud.com>
2018-05-20 15:27:28 -0700

    Shell script example
    An example regarding the 'Update' section in README

---
+++ rmp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x70000000E,"HIDKeyboardModifierMappingDst":0x7000000E5},{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x70000000E}]}'
+
